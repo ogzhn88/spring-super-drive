bca11422dcb8ffc3b552a81b9e98078ff50be53c
oguzhan.unalir <oguzhanunalir@hotmail.com>
2020-09-15 00:55:40 +0300

    commit

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -24,10 +24,6 @@ class CloudStorageApplicationTests {
 	private LoginPage loginPage;
 	private SignupPage signupPage;
 	private HomePage homePage;
-
-
-
-
 	private WebDriver driver;
 
 	@BeforeAll
